1cd4ddb299f16022e54893ef4e74d2fc15b64a24
jh96 <36338184+jordan-hoang@users.noreply.github.com>
2025-10-23 16:45:25 -0700

    basic setup of boost and main.cpp done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
-int main()
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "Show help message")
+        ("name,n", po::value<std::string>(), "Your name");
+
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // 3. Handle options
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+
+    if (vm.count("name")) {
+        std::cout << "Hello, " << vm["name"].as<std::string>() << "!" << std::endl;
+    } else {
+        std::cout << "Hello, world!!!" << std::endl;
+    }
+
+
+
     return 0;
 }
