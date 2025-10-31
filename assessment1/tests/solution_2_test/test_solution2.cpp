bed96905a413c26504cdbf7c38e4c0eff624f892
jh96 <36338184+jordan-hoang@users.noreply.github.com>
2025-10-31 16:31:53 -0700

    added a proper test for solution #2

--- assessment1/tests/solution_2_test/test_solution2.cpp
+++ assessment1/tests/solution_2_test/test_solution2.cpp
@@ -63,6 +63,59 @@ TEST(SOLUTION_2_TESTING, myTest) {
 }
 
 
+TEST(SOLUTION_2_TESTING, proper) {
+
+    ASSERT_FALSE(G_INPUTFILE.empty()) << "File path not set! Did you pass it from CMake?";
+    std::string filePath = "../../../data/testjson/sample_json_1.json" ;
+
+    std::string conn_string =
+        "host=localhost "
+        "port=5432 "      // Standard PostgreSQL port
+        "user=postgres "
+        "password=Moonshine4me " /// REPLACE WITH YOUR PASSWORD!!!
+        "dbname=postgres ";      /// Generic default database.
+
+    json parsed_json;
+    EXPECT_NO_THROW({
+         parsed_json = ResultFileIO::readJsonFile(filePath);
+    });
+
+    QueryFileStructure query_struct = QueryFileJsonParser::from_json(parsed_json);
+
+    EXPECT_EQ(query_struct.valid_region.p_min.x, 0) << "Invalid p_min, for file " << filePath << "\n";
+    EXPECT_EQ(query_struct.valid_region.p_min.y, 0) << "Invalid p_min, for file " << filePath << "\n";
+    EXPECT_EQ(query_struct.valid_region.p_max.x, 610) << "Invalid p_max, for file " << filePath << "\n";
+    EXPECT_EQ(query_struct.valid_region.p_max.y, 600) << "Invalid p_max, for file " << filePath << "\n";
+
+    EXPECT_EQ(query_struct.operator_crop.region.p_min.x, 0) << "Invalid operator_crop.p_min.x, for file " << filePath << "\n";
+    EXPECT_EQ(query_struct.operator_crop.region.p_min.y, 0) << "Invalid operator_crop.p_min.y, for file " << filePath << "\n";
+    EXPECT_EQ(query_struct.operator_crop.region.p_max.x, 2000) << "Invalid operator_crop.p_max.x, for file " << filePath << "\n";
+    EXPECT_EQ(query_struct.operator_crop.region.p_max.y, 2000) << "Invalid operator_crop.p_max.y, for file " << filePath << "\n";
+
+    EXPECT_EQ(query_struct.operator_crop.category, 2) << "Invalid Category value " << filePath << "\n";
+    EXPECT_EQ(query_struct.operator_crop.proper, true) << "Invalid Proper Value " << filePath << "\n";
+
+
+    std::set<int64_t> expected = {0};
+    EXPECT_EQ(query_struct.operator_crop.one_of_groups, expected) << "Invalid Proper Value " << filePath << "\n";
+
+
+    std::vector<InspectionRegion> list_records;
+    EXPECT_NO_THROW({
+        list_records = InspectionRegion::readRecordsFromDB(conn_string); // Fetch all the database results.
+    });
+
+    EXPECT_EQ(list_records.size(), 10); // Unfiltered
+
+    std::vector<InspectionRegion> filtered_records = query_struct.filterWithQueryStruct(list_records, false);
+    EXPECT_EQ(filtered_records.size(), 0); // All points should be removed from set. so empty.
+
+
+}
+
+
+
+
 
 // Optional: demonstrate passing a command-line argument
 int main(int argc, char **argv) {
